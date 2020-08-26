616b8445bfc5c5eb89c3977464d51b5b89aee92b
LHerdy <le.herdy@gmail.com>
2020-08-26 14:59:38 -0300

    Finished pager.

--- blog/views.py
+++ blog/views.py
@@ -6,23 +6,12 @@ from django.core.paginator import Paginator, EmptyPage,\
 from django.views.generic import ListView
 from .models import Post
 
- # Paginator
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                 'blog/post/list.html',
-                 {'page': page,
-                  'posts': posts})
+# Paginator
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4 # 4 posts in each page
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -36,8 +25,4 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+
