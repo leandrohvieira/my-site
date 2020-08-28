7414206ac8906df7084048bf135d215ab81277ed
LHerdy <le.herdy@gmail.com>
2020-08-27 23:11:15 -0300

    Custom template tags finalized.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def total_posts():
     return Post.published.count()
 
-@register.inclusion_tag('blog/post/latest_poss.html')
+@register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
